2169c425b4bf9508af9ab75abcb52ecd041e9518
nameki <nameki149@gmail.com>
2024-05-11 10:45:21 +0900

    ハンバーガーメニューの追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,19 +15,10 @@ module.exports = {
       // ブレイクポイントの修正
       screens: {
         sm: "640px",
-        // => @media (min-width: 640px) { ... }
-
         md: "768px",
-        // => @media (min-width: 768px) { ... }
-
         lg: "1024px",
-        // => @media (min-width: 1024px) { ... }
-
-        xl: "1280px",
-        // => @media (min-width: 1280px) { ... }
-
-        "2xl": "1280px",
-        // => @media (min-width: 1280px) { ... }
+        xl: "1120px",
+        "2xl": "1120px",
       },
     },
   },
