69b121e8a81e1082f72a0342f9279073a10a677d
nameki <nameki149@gmail.com>
2024-06-02 11:11:28 +0900

    保存先フォルダを再帰的に作成する

--- src/utils/saveImage.js
+++ src/utils/saveImage.js
@@ -2,18 +2,8 @@ import fs from "fs";
 
 export async function saveImage(url, fileName, destinationPath) {
   // 保存先フォルダの作成
-  if (!fs.existsSync("public/")) {
-    fs.mkdirSync("public/");
-  }
-  if (!fs.existsSync("public/articleImages/")) {
-    fs.mkdirSync("public/articleImages/");
-  }
-  if (!fs.existsSync("public/bookImages/")) {
-    fs.mkdirSync("public/bookImages/");
-  }
-
   if (!fs.existsSync(destinationPath)) {
-    fs.mkdirSync(destinationPath);
+    fs.mkdirSync(destinationPath, { recursive: true });
   }
   // 画像をblobで取得する
   const response = await fetch(url);
